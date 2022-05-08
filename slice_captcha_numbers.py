440377e1726a621e1ba6234330d8f2b5c45af5ea
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2022-05-08 13:53:18 +0200

    slices

--- slice_captcha_numbers.py
+++ slice_captcha_numbers.py
@@ -2,6 +2,8 @@ from PIL import Image
 from typing import List, Optional, Tuple
 
 black_threshold = 50
+captcha_character_count = 8
+captcha_slice_width = 10
 
 def slice_captcha_numbers(image_path: str) -> List[str]:
     captcha_image = Image.open(image_path)
@@ -9,10 +11,10 @@ def slice_captcha_numbers(image_path: str) -> List[str]:
     first_black_pixel = get_first_black_pixel_coordinates(captcha_image)
     if first_black_pixel == None:
         raise InvalidImageExceptionclass()
-        
-    print(first_black_pixel)
 
-    return []
+    slicing_start_x_coordinate = figure_out_slicing_start_x_coordinate(first_black_pixel)
+
+    return get_image_slices(captcha_image, slicing_start_x_coordinate, image_path[0: -4])
 
 class InvalidImageExceptionclass(Exception):
     pass
@@ -28,3 +30,24 @@ def get_first_black_pixel_coordinates(image: Image) -> Optional[Tuple[int]]:
                 return pixel_coordinates
 
     return None
+
+def figure_out_slicing_start_x_coordinate(first_black_pixel: Tuple[int]) -> int:
+    return first_black_pixel[0] - 1
+
+def get_image_slices(image: Image, slicing_start_x_coordinate: int, slice_image_name_prefix: str) -> List[str]:
+    slices: List[str] = []
+    height = image.size[1]
+
+    for captcha_character_index in range(0, captcha_character_count):
+        slice_offset = captcha_slice_width * captcha_character_index
+        slice_box = (
+            slicing_start_x_coordinate + slice_offset,
+            0,
+            slicing_start_x_coordinate + captcha_slice_width + slice_offset,
+            height
+        )
+        slice_file_name = slice_image_name_prefix + '_' + str(captcha_character_index) + '.png'
+        image.crop(slice_box).save(slice_file_name)
+        slices.append(slice_file_name)
+
+    return slices
