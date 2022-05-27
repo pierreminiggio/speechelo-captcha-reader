19b95e6da703e25b6fd69594b217aad06b1e16fb
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-05-27 15:28:07 +0200

    Update slice_captcha_numbers.py

--- slice_captcha_numbers.py
+++ slice_captcha_numbers.py
@@ -5,6 +5,7 @@ from typing import List, Optional, Tuple
 black_threshold = 50
 captcha_character_count = 8
 captcha_slice_width = 10
+captcha_slice_height = 12
 
 def slice_captcha_numbers(image_path: str) -> List[str]:
     captcha_image = Image.open(image_path).convert('RGB')
@@ -39,13 +40,31 @@ def is_black_pixel(pixel_color: Tuple[int]) -> bool:
 
 def figure_out_slicing_start_x_coordinate(first_black_pixel: Tuple[int], image: Image, slice_image_name_prefix: str) -> int:
     normal_slicing_start_x_coordinate = first_black_pixel[0] - 1
-    
-    return normal_slicing_start_x_coordinate - 1 if is_back_pixel_part_of_number_1(image, normal_slicing_start_x_coordinate, slice_image_name_prefix) else normal_slicing_start_x_coordinate
-
-def is_back_pixel_part_of_number_1(image: Image, normal_slicing_start_x_coordinate: int, slice_image_name_prefix: str) -> bool:
     image_path = get_single_image_slice(image, normal_slicing_start_x_coordinate, slice_image_name_prefix, 0)
+    single_number = read_single_number(image_path)
 
-    return read_captcha_number(image_path) == '1'
+    if single_number == '1':
+        return normal_slicing_start_x_coordinate - 1
+
+    if single_number == '0':
+        if are_last_2_columns_empty(image_path):
+            return normal_slicing_start_x_coordinate - 1
+
+    return normal_slicing_start_x_coordinate
+
+def read_single_number(image_path: str) -> str: return read_captcha_number(image_path)
+
+def are_last_2_columns_empty(image_path: str) -> bool:
+    slice_image = Image.open(image_path).convert('RGB')
+    (width, height) = slice_image.size
+
+    for x in range(width - 2, width):
+        for y in range(0, height):
+            pixel_coordinates = (x, y)
+            pixel_color = slice_image.getpixel(pixel_coordinates)
+            if is_black_pixel(pixel_color): return False
+
+    return True
 
 def get_image_slices(image: Image, slicing_start_x_coordinate: int, slice_image_name_prefix: str) -> List[str]:
     slices: List[str] = []
@@ -58,14 +77,44 @@ def get_image_slices(image: Image, slicing_start_x_coordinate: int, slice_image_
 
 def get_single_image_slice(image: Image, slice_offset: int, slice_image_name_prefix: str, captcha_character_index: int) -> str:
     height = image.size[1]
+    slice_baseline = find_slice_baseline(image, slice_offset)
+
+    image_x_start = slice_offset
+    image_y_start = slice_baseline
+    image_x_end = image_x_start + captcha_slice_width
+    image_y_end = image_y_start + captcha_slice_height
+
+    # Remove noise
+    for x in range(image_x_start, image_x_end):
+        for y in range(image_y_start, image_y_end):
+            pixel_coordinates = (x, y)
+            pixel_color = image.getpixel(pixel_coordinates)
+            new_color = (0, 0, 0) if is_black_pixel(pixel_color) else (255, 255, 255)
+            image.putpixel(pixel_coordinates, new_color)
 
     slice_box = (
-        slice_offset,
-        0,
-        slice_offset + captcha_slice_width,
-        height
+        image_x_start,
+        image_y_start,
+        image_x_end,
+        image_y_end
     )
+
     slice_file_name = slice_image_name_prefix + '_' + str(captcha_character_index) + '.png'
     image.crop(slice_box).save(slice_file_name)
 
     return slice_file_name
+
+def find_slice_baseline(image: Image, slice_offset: int) -> int:
+    height = image.size[1]
+
+    for reverse_y in range(height):
+        y = height - reverse_y - 1
+        for x in range(slice_offset, slice_offset + captcha_slice_width):
+            pixel_coordinates = (x, y)
+            pixel_color = image.getpixel(pixel_coordinates)
+
+            if is_black_pixel(pixel_color):
+                return y - captcha_slice_height + 2
+
+    raise Exception('No slice baseline found')
+    
