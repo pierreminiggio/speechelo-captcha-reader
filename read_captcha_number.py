cfbad578dfe8e8a8030d194cbf08c148fedd678f
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-05-27 15:27:40 +0200

    Update read_captcha_number.py

--- read_captcha_number.py
+++ read_captcha_number.py
@@ -2,6 +2,23 @@ from pytesseract import image_to_string
 from cv2 import imread
 
 def read_captcha_number(file_path: str) -> str:
-    read_character = image_to_string(imread(file_path), config='--psm 7 -c tessedit_char_whitelist=oO0123456789').strip().replace('o', '0').replace('O', '0')
-    
-    return read_character if read_character != '' else '0'
+    image = imread(file_path)
+    read_character = image_to_string(image, config='--psm 7 -c tessedit_char_whitelist=oO0123456789').strip().replace('o', '0').replace('O', '0')
+
+    if read_character == '00':
+        return '0'
+
+    if read_character == '':
+        return '0'
+
+    if read_character == '4':
+        check_non_4_char = image_to_string(image, config='--psm 7 -c tessedit_char_blacklist=4').strip()
+
+        if check_non_4_char == 'A': return '7'
+
+    if read_character == '3':
+        check_non_3_char = image_to_string(image, config='--psm 7 -c tessedit_char_blacklist=3').strip()
+
+        if check_non_3_char == '5)': return '5'
+
+    return read_character
